effc535256b3f0fa3a5f6b695a67da01ada2c5ef
gitnickolson <nicknickolson@web.de>
2023-11-02 14:03:14 +0100

    Add game loop

---
+++ lib/game.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+require_relative 'player'
+require_relative 'board'
+
+class Game
+  attr_accessor :player, :board, :player_number, :game_turn, :game_ended
+
+  def initialize
+    @player = Player.new
+    @board = Board.new
+    @player_number = 1
+    @game_turn = 0
+    @game_ended = false
+  end
+
+  def start
+    board.print_board
+    puts "Welcome to \"Connect Four\"! The player that wants to play with blue chips shall start!"
+    puts "To play, just enter a number ranging from 1 to 7 to place your chip at the specified number."
+
+    unless game_ended
+      game_loop
+    end
+  end
+
+  def game_loop
+    if (game_turn % 2).even?
+      player_number = 1
+    else
+      player_number = 2
+    end
+
+    player_color = player.get_color(player_number)
+    player_input = gets.chomp.to_i
+    check_input(player_input)
+
+    if board.update_board(player_input, player_color) == nil
+      game_loop
+    end
+
+    @game_turn += 1
+  end
+
+  def check_input(input)
+    if !(1..7).include?(input)
+      puts "Error: Please enter a number ranging from 1 to 7"
+      game_loop
+    end
+  end
+end
---
+++ lib/player.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Player
+  def get_color(number)
+    if @number == 1
+      X
+    elsif @number == 2
+      Y
+    end
+  end
+end
