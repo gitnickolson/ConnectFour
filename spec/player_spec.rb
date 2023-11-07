6f110b40e336afbf1a04854425760dfec4395e69
gitnickolson <nicknickolson@web.de>
2023-11-07 13:24:43 +0100

    Add tests for player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,17 +3,25 @@
 require './lib/player'
 
 describe Player do
+  let(:player) { Player.new(1) }
+
   describe '#get_color' do
-    it 'Returns blue when 1 is the argument' do
-      player = Player.new
+    it 'returns blue when 1 is the argument' do
       expect(player.get_color(1)).to eql(:blue)
     end
   end
 
   describe '#get_color' do
-    it 'Returns red when 2 is the argument' do
-      player = Player.new
+    it 'returns red when 2 is the argument' do
       expect(player.get_color(2)).to eql(:red)
     end
   end
+
+  describe '#get_color' do
+    it 'returns false if color is not valid' do
+      expect(player.get_color(32)).to eql(nil)
+    end
+  end
+
+  # get_input muss noch vertestet werden
 end
