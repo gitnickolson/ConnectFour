210fbe49a2c971d0549514d10ff6f403f44f4c18
gitnickolson <nicknickolson@web.de>
2023-11-09 08:30:47 +0100

    Add blue player spec file

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class Player
-  attr_accessor :player_number
-
-  def initialize(player_number)
-    @player_number = player_number
-  end
 
   def get_color(number)
     return unless is_number_valid?(number)
