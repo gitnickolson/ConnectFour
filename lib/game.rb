0cd7475c5ea07ad8ebeb8c78be20ece3196754b6
gitnickolson <nicknickolson@web.de>
2023-11-09 16:03:48 +0100

    Finish tests for start method in game

--- lib/game.rb
+++ lib/game.rb
@@ -12,9 +12,22 @@ class Game
 
   def start
     board.print
+
+    puts "Welcome to Connect Four! The player that wants to play with blue chips shall start!
+To play, just enter a number ranging from 1 to 7 to place your chip in the specified column."
+
+    start_loop
   end
 
   private
+
+  def start_loop
+    loop
+  end
+
+  def loop
+    #stuff
+  end
 
   attr_reader :board
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -26,5 +26,30 @@ RSpec.describe Game do
       game.start
       expect(board).to have_received(:print)
     end
+
+    it 'displays welcome message' do
+      expected_board_output = <<~BOARD
+        1  ┃  | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ |  ┃
+        2  ┃  | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ |  ┃
+        3  ┃  | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ |  ┃
+        4  ┃  | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ |  ┃
+        5  ┃  | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ |  ┃
+        6  ┃  | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ | ⚫️ |  ┃
+         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+                 1    2    3    4    5    6    7
+      BOARD
+
+      expect do
+        game.start
+      end.to output("#{expected_board_output}Welcome to Connect Four! The player that wants to play with blue chips shall start!
+To play, just enter a number ranging from 1 to 7 to place your chip in the specified column.\n").to_stdout
+    end
+
+    it 'starts the game loop' do
+      allow(game).to receive(:start_loop)
+
+      game.start
+      expect(game).to have_received(:start_loop)
+    end
   end
 end
